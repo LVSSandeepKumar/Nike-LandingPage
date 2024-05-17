c81ea6f1c4bd3efbb72aa9509a63231f34adbcdb
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-05-17 13:06:09 +0530

    Subscribe Section Updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
-const Button = ({label, iconURL, backgroundColor, textColor, borderColor}) => {
+const Button = ({label, iconURL, backgroundColor, textColor, borderColor, fullWidth}) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-monsterrat rounded-full text-lg leading-none
       ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : `bg-coral-red text-white border-coral-red ` }
+      ${fullWidth && 'w-full'}
      `}>
         {label}
         {iconURL && <img 
